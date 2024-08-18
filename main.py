3c3f01c02cc05265b682f535bae7a3fc79b80bc8
Dainis <gatesby@protonmail.com>
2024-08-18 17:29:48 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,147 @@
+from discord import app_commands
+from discord.utils import get # New import
+from discord.ext import commands
+from random import randint
+from discord.ext import commands, tasks
+import discord
+import os
+import asyncio
+import requests
+
+TOKEN = 'MTI3MjI3MDc4MzQyMjUzMzcxMw.GzVVEr._RqefcWtLHIeeCnEhIvArnU9uYoOfi9jBNWsVI'
+
+intents = discord.Intents.all()
+intents.members = True
+bot = commands.Bot(command_prefix="(", intents=intents)
+
+xertuncord = 1114565796404928594
+ragecord = 1219008436428345528 
+swagballs = 938728183203758080
+
+tree = bot.tree
+
+@bot.event
+async def on_ready():
+    sent_today = False
+    print(f'We have logged in as {bot.user}')
+    await tree.sync(guild=discord.Object(swagballs))
+    await tree.sync(guild=discord.Object(ragecord))
+    await tree.sync(guild=discord.Object(xertuncord))
+
+@bot.listen()
+async def on_message(message):
+    if message.author.bot:
+        return
+    
+    bridgeSB_1 = bot.get_channel(1274386678831644753)  # Ragecord
+    bridgeSB_2 = bot.get_channel(1274458307716845608)  # SwagCord
+    bridgeSB_3 = bot.get_channel(1274668145243586673)  # Xertuncord
+    
+    async def send_to_others(original_channel, embed=None):
+        if original_channel != bridgeSB_1:
+            await bridgeSB_1.send(embed=embed)
+        if original_channel != bridgeSB_2:
+            await bridgeSB_2.send(embed=embed)
+        if original_channel != bridgeSB_3:
+            await bridgeSB_3.send(embed=embed)
+        
+    if message.channel in (bridgeSB_1, bridgeSB_2, bridgeSB_3):
+        embed_color = 0xffffff  # Default color
+        if message.channel == bridgeSB_1: #ragecord
+            embed_color = 0xffffff
+        elif message.channel == bridgeSB_2: #swagcord
+            embed_color = 0xffdf00
+        elif message.channel == bridgeSB_3: #xertuncord
+            embed_color = 0xf78eff
+        #elif message.channel == bridgeSB_3: #wunkcord
+         #   embed_color = 0x00ffcc
+
+        embed = discord.Embed(title='SOE "SwagBalls Passenger Train"', colour=embed_color)
+        embed.add_field(name=("Passenger from " + str(message.guild)), value=str(message.author), inline=True)
+        embed.set_footer(text="Connecting the Swagosphere, one train at a time.")
+        
+        if not message.attachments and not message.stickers:
+            embed.add_field(name="Message", value=str(message.clean_content), inline=False)
+            await send_to_others(message.channel, embed=embed)
+        
+        elif message.attachments and not message.stickers:
+            attachments = message.attachments[0]
+            embed.set_image(url=attachments.url)
+            await send_to_others(message.channel, embed=embed)
+        
+        elif not message.attachments and message.stickers:
+            sticker = message.stickers[0]
+            embed.set_image(url=sticker.url)
+            await send_to_others(message.channel, embed=embed)
+            
+
+@tree.command(
+    name="ticket",
+    description="Buy timed tickets for the SwagBalls Passanger Train",
+    guilds=[discord.Object(swagballs), discord.Object(ragecord), discord.Object(xertuncord)],
+)
+@app_commands.describe(mins="How many minutes do you want your ticket to be usable for. 1min:1000UBUX ratio.")
+async def tickets(interaction: discord.Interaction, mins: int):
+    # Deducting
+    url = f"https://unbelievaboat.com/api/v1/guilds/{interaction.guild_id}/users/{interaction.user.id}"
+    if mins <= 0:
+        await interaction.response.send_message("```INVALID AMOUNT\nPlease enter valid amount.```")
+    else:
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfaWQiOiIxMjcyMzMyMzM5NjkwNzMzNTk2IiwiaWF0IjoxNzIzNDE4MDY4fQ.h5gGc4ZRWOcH1DkYlB-brvUAR91Sij1zckoQdvxG5eU"
+        }
+        responseC = requests.get(url, headers=headers) 
+        bal = responseC.json().get("cash") #gets balance
+
+        if bal >= 1000 * mins: #checks if you have enough
+            payload = {
+                "reason": "TRAINTICKET\"SBTRAIN\"\"",
+                "cash": -1000 * mins
+            }
+            
+            response = requests.patch(url, json=payload, headers=headers)
+            
+            url = "https://unbelievaboat.com/api/v1/guilds/938728183203758080/users/432437043956809738"
+            
+            payload = {
+                "reason": "TRAINTICKET\"SBTRAIN\"\"",
+                "bank": 1000 * mins
+            }
+            headers = {
+                "accept": "application/json",
+                "content-type": "application/json",
+                "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfaWQiOiIxMjcyMzMyMzM5NjkwNzMzNTk2IiwiaWF0IjoxNzIzNDE4MDY4fQ.h5gGc4ZRWOcH1DkYlB-brvUAR91Sij1zckoQdvxG5eU"
+            }
+            response = requests.patch(url, json=payload, headers=headers)
+
+            
+            def TicketRoleIdDetermine():
+                if interaction.guild_id == swagballs:
+                    return 1274461298960105543
+                elif interaction.guild_id == ragecord:
+                    return 1274385477126062182
+                elif interaction.guild_id == xertuncord:
+                	return 1274668418506686527
+            
+            def StationChannelDetermine():
+                if interaction.guild_id == swagballs:
+                    return 1274458307716845608
+                elif interaction.guild_id == ragecord:
+                    return 1274386678831644753
+                elif interaction.guild_id == xertuncord:
+                    return 1274668145243586673
+            
+            await interaction.response.send_message(f"### Transaction succesful!\nYou bought {mins} ticket(s) to use in the <#{StationChannelDetermine()}> station.\n{1000 * mins} UBUX have been deducted from your cash account.\n```SENDER: {interaction.user}\nBENEFACTOR(S): 90% - SOE.SWAG.PSNG.TRAN///10% - AWSM.SCE.CNGL.LTD\nAMOUNT: {1000 * mins}.\nTHANK YOU FOR USING OUR SERVICES!```")
+            await interaction.user.add_roles(discord.Object(TicketRoleIdDetermine()))
+            await asyncio.sleep(mins * 60)
+            await interaction.user.remove_roles(discord.Object(TicketRoleIdDetermine()))
+            await interaction.followup.send(f"{interaction.user.mention}, your ticket has expired.", ephemeral=True)
+            
+            me = bot.get_user(432437043956809738)
+            await me.send(f'{1000 * mins} acquired.')
+        else:
+            await interaction.response.send_message("```LOW BALANCE\nPlease try lower amount.\n-# This might mean that you dont have enough **cash**, so make sure to withdraw some!```")
+        
+bot.run(TOKEN)
